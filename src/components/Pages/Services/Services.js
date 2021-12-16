c1ab9175d54ff2b5242f70a629571cbcec47e8d9
jobayercse71 <jobayercse71.com>
2021-12-16 13:16:55 +0600

    service-spinner

--- src/components/Pages/Services/Services.js
+++ src/components/Pages/Services/Services.js
@@ -1,55 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Services.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Services.css";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
 
-    const [services,setServices]=useState([]);
- 
-
-    useEffect(()=>{
-        fetch('https://protected-stream-55313.herokuapp.com/services')
-        .then(res=>res.json())
-        .then(data=>setServices(data.slice(0,6)))
-    },[]);
-
-   
-
-    return (
-        <div className="services-section">
-        <h1 className="py-5 text-center"><span className="text-danger">Our</span> Services</h1>
-        <div className="container">
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-               
-              
-
-                {
-                    services.map(service => <div key={service._id} className="col">
-                        <div className="card h-100">
-                            <img src={service.img} className="card-img-top services-img" alt="..." />
-                        <div className="card-body">
-                                <h5 className="card-title">{service.name}</h5>
-                                <h6 className="text-dark">Price:$ {service.price}</h6>
-                                <p className="card-text">{service.description}</p>
-                            </div>
-                            <div className="">
-                           
-                                <Link  to={`/carDetails/${service._id}`}>
-                                    <button className="btn btn-danger fs-6 m-2">Purchase</button>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    )
-                }
+  useEffect(() => {
+    fetch("https://protected-stream-55313.herokuapp.com/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data.slice(0, 6)));
+  }, []);
 
 
+  return (
+    <div className="services-section">
+      <h1 className="py-5 text-center">
+        <span className="text-danger">Our</span> Services
+        <br/>
+
+        {
+            services.length==0 && <div class="spinner-grow text-danger" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        }
+      </h1>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+
+          { services.map((service) => (
+            <div key={service._id} className="col">
+              <div className="card h-100">
+                <img
+                  src={service.img}
+                  className="card-img-top services-img"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{service.name}</h5>
+                  <h6 className="text-dark">Price:$ {service.price}</h6>
+                  <p className="card-text">{service.description}</p>
+                </div>
+                <div className="">
+                  <Link to={`/carDetails/${service._id}`}>
+                    <button className="btn btn-danger fs-6 m-2">
+                      Purchase
+                    </button>
+                  </Link>
+                </div>
+              </div>
             </div>
+          ))}
+
+          
         </div>
+      </div>
     </div>
-
-
-    );
+  );
 };
 
 export default Services;
